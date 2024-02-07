6f9b42b777e5e00e7619e63886904535a04f576c
balash <alibalash.dev@gmail.com>
2024-02-07 13:14:25 +0330

    add menu bar and gitignore files

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
-# Create your views here.
-def index(request):
-    return render(request , 'core/index.html')
+# Create your views here. 
+def index(request): 
+    return render(request , 'core/index.html') 
+# render contact page html 
+def contact(request): 
+    return render(request , 'core/contact.html')
